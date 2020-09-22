427892af6dc2bd6ae6fc6df0feb38213c2065435
Pavel Ponomarev <pavponn@yandex.ru>
2020-09-23 00:03:48 +0300

    Introduce theme settings

--- Weather/app/src/main/java/com/example/weather/MainActivity.kt
+++ Weather/app/src/main/java/com/example/weather/MainActivity.kt
@@ -2,15 +2,68 @@ package com.example.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.example.weather.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mBinding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    companion object {
+        const val THEME_KEY = "THEME_KEY"
     }
+
+    enum class Theme { LIGHT, DARK }
+
+    private var currentTheme = Theme.LIGHT
+    private lateinit var mainActivityBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        currentTheme = savedInstanceState?.getSerializable(THEME_KEY) as Theme? ?: Theme.LIGHT
+        updateTheme()
+        super.onCreate(savedInstanceState)
+        mainActivityBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        updateAccordingToTheme()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable(THEME_KEY, currentTheme)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        currentTheme = savedInstanceState.getSerializable(THEME_KEY) as Theme
+    }
+
+    fun onChangeThemeClick(v: View) {
+        currentTheme = when(currentTheme) {
+            Theme.LIGHT -> Theme.DARK
+            Theme.DARK -> Theme.LIGHT
+        }
+        recreate()
+    }
+
+    private fun updateTheme() {
+        when (currentTheme) {
+            Theme.LIGHT -> {
+                setTheme(R.style.LightTheme)
+            }
+            Theme.DARK -> {
+                setTheme(R.style.DarkTheme)
+            }
+        }
+    }
+
+    private fun updateAccordingToTheme() {
+        when (currentTheme) {
+            Theme.LIGHT -> {
+                themeButton.setImageResource(R.drawable.ic_theme_night)
+            }
+            Theme.DARK -> {
+                themeButton.setImageResource(R.drawable.ic_theme_day)
+            }
+        }
+    }
+
+
 }
